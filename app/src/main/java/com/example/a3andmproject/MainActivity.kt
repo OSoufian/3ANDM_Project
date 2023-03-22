9cf1c25c44a83de9f2c89ca838d1304e16dba321
OSoufian <osoufian@hotmail.fr>
2023-03-22 16:18:15 +0100

    Page de chargement + Création logo

---
+++ app/src/main/java/com/example/a3andmproject/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.a3andmproject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val timer = Timer(false).schedule(object: TimerTask(){
+            override fun run() {
+                val intentHome = Intent(this@MainActivity, HomeActivity::class.java);
+                startActivity(intentHome);
+            }
+        }, 2000);
+    }
+}
