57e775e05c104e19d71944493c735a51c085fb52
OSoufian <osoufian@hotmail.fr>
2023-03-26 00:10:45 +0100

    Affichage des recettes (titres et images) sur la Homepage

--- app/src/main/java/com/example/a3andmproject/HomeActivity.kt
+++ app/src/main/java/com/example/a3andmproject/HomeActivity.kt
@@ -2,45 +2,95 @@ package com.example.a3andmproject
 
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.android.volley.AuthFailureError
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
+import com.example.a3andmproject.models.Food2ForkApi
+import com.example.a3andmproject.models.Recipe
 import com.example.a3andmproject.utils.Constant
+import com.google.gson.Gson
 
 class HomeActivity : AppCompatActivity() {
-    private val apiButton: Button by lazy { findViewById<Button>(R.id.apiButton) }
+    private val recipeListRecyclerView: RecyclerView by lazy { findViewById<RecyclerView>(R.id.recipeListRecyclerView) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        apiButton.setOnClickListener {
+        makeRequest()
+    }
 
-            val queue = Volley.newRequestQueue(this)
-            val url = Constant.URL
+    private fun makeRequest() {
 
-            val getRequest = object : StringRequest(
-                Request.Method.GET, url,
-                Response.Listener<String> { response ->
-                    // response
-                    Log.d("Response", response)
-                },
-                Response.ErrorListener { error ->
-                    Log.d("ERROR","error => " + error.toString())
-                }
-            ){
-                @Throws(AuthFailureError::class)
-                override fun getHeaders(): Map<String, String> {
-                    val params: MutableMap<String, String> = HashMap()
-                    params["Authorization"] = "Token 9c8b06d329136da358c2d00e76946b0111ce2c48"
-                    return params
-                }
+        val queue = Volley.newRequestQueue(this)
+        val url = Constant.URL
+
+        val getRequest = object : StringRequest(
+            Method.GET, url,
+            Response.Listener { json ->
+                Log.d("json", json)
+                val api = Gson().fromJson(json, Food2ForkApi::class.java)
+                Log.d("api", api.results.toString())
+
+                recipeListRecyclerView.layoutManager = LinearLayoutManager(this)
+                val adapter = RecipeAdapter(api.results)
+                recipeListRecyclerView.adapter = adapter
+            },
+            Response.ErrorListener { error ->
+                Log.e("Error","error => " + error.toString())
             }
+        ){
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String> {
+                val params: MutableMap<String, String> = HashMap()
+                params["Authorization"] = "Token 9c8b06d329136da358c2d00e76946b0111ce2c48"
+                return params
+            }
+        }
+        queue.add(getRequest)
+    }
+    inner class RecipeAdapter(private val recipes: List<Recipe>) : RecyclerView.Adapter<RecipeAdapter.RecipeViewHolder>() {
 
-            queue.add(getRequest)
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeViewHolder {
+            val view =
+                LayoutInflater.from(parent.context).inflate(R.layout.recipe_item, parent, false)
+            return RecipeViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: RecipeViewHolder, position: Int) {
+            val recipe = recipes[position]
+            holder.bind(recipe)
+        }
+
+        override fun getItemCount(): Int {
+            return recipes.size
+        }
+
+        inner class RecipeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+
+            private val titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
+            private val recipeImageView: ImageView = itemView.findViewById(R.id.recipeImageView)
+
+            fun bind(recipe: Recipe) {
+                titleTextView.text = recipe.title
+
+                Glide.with(itemView.context)
+                    .load(recipe.featured_image)
+                    .placeholder(R.drawable.placeholder)
+                    .into(recipeImageView)
+            }
         }
     }
 }
---
+++ app/src/main/java/com/example/a3andmproject/models/Food2ForkApi.kt
@@ -0,0 +1,24 @@
+package com.example.a3andmproject.models
+
+data class Food2ForkApi(
+    val count: Int,
+    val next: String?,
+    val previous: String?,
+    val results: List<Recipe>
+)
+
+data class Recipe(
+    val pk: Int,
+    val title: String,
+    val publisher: String,
+    val featured_image: String,
+    val rating: Int,
+    val source_url: String,
+    val description: String?,
+    val cooking_instructions: String?,
+    val ingredients: List<String>,
+    val date_added: String,
+    val date_updated: String,
+    val long_date_added: Long,
+    val long_date_updated: Long
+)
